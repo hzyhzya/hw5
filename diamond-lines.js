37cad65ca27a283c99a6208797943d6012851f60
hzyhzya <55303885+hzyhzya@users.noreply.github.com>
2019-10-12 21:52:08 -0700

    Create diamond-lines.js

---
+++ diamond-lines.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(200, i, 200+i, i);
+  }
+  
+   for (var i = 10; i <= 200; i = i + 10) {
+    line(200, i, 200-i, i);
+  }
+  
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(200, 400-i, 200+i, 400-i);
+  }
+  
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(200, 400-i, 200-i, 400-i);
+  }
